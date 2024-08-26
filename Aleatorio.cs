09a782dc169bd75e9c1737fc12b7ea555a857f1c
diegomg29 <111083069+diegomg29@users.noreply.github.com>
2024-08-26 15:59:01 -0600

    Add files via upload

---
+++ Aleatorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1.Clases
+{
+    public class Aleatorio
+    {
+        public long Id { get; set; }
+        public int Valor { get; set; }
+        public int Posicion { get; set; }
+        public Aleatorio() { }
+    }
+}
---
+++ Simulacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormsApp1.Clases;
+
+namespace WinFormsApp1.Algoritmos
+{
+    public class Simulacion
+    {
+        public Simulacion() { }
+        public List<int> GeneradorValores(int n)
+        {
+            List<int> listaAleatorios = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                listaAleatorios.Add(5*(i+1));
+            }
+            return listaAleatorios;
+
+        }
+    }
+}
